796e57616ca0fd7f17452f7fe0d0fc00b765542b
juned-dev <juned.dhanga@gmail.com>
2020-08-14 08:29:44 +0530

    landing and welcome page html done

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+const plugins = [
+    require("tailwindcss"),
+    require("autoprefixer")
+
+];
+
+if(process.env.NODE_ENV == "production"){
+    const purgecss = require("@fullhuman/postcss-purgecss")({
+        content: [
+            "*.html"
+        ],
+
+        defaultExtractor: function(content){
+            return content.match(/[\w-/:]+(?<!:)/g)  || [];
+        }
+    });
+
+    plugins.push(purgecss);
+
+    const cssnano = require("cssnano")({
+        preset : "default"
+    });
+    
+    plugins.push(cssnano);
+}
+
+module.exports = {
+    plugins: plugins
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5ac3f4',
+        secondary: '#3d1099',
+        grayLight: '#7d7d7d',
+        grayDark: '#5a5a5a',
+        formBg: '#4b23a1'
+      },
+
+      fontFamily:{
+        body: ['Poppins', 'sans-serif']
+      },
+
+      container: {
+        screens: {
+          lg: "1360px"
+        },
+        center: true
+      }
+
+    },
+
+    
+  },
+  variants: {},
+  plugins: [],
+}
